b9b6707c38611ec361718b9e29b1a2eb60b7c122
MattSegal <mattdsegal@gmail.com>
2017-10-21 18:27:47 +1100

    add linter>

--- Hellhole/shortcuts.ps1
+++ Hellhole/shortcuts.ps1
@@ -2,7 +2,7 @@ param([String]$command)
 
 function IsInstalled {
     param([String]$cmd)
-    $ErrorActionPreference = "SilentlyContinue"
+    $ErrorActionPreference = "Ignore"
     $installed =  Get-Command $cmd
     $ErrorActionPreference = "Stop"
     return $cmd
@@ -10,50 +10,63 @@ function IsInstalled {
 
 switch ($command) {
     'setup' {
-        Write-Host "Setting up Hellhole environment...`n"
+        Write-Host "`nSetting up Hellhole environment..."
         $ErrorActionPreference = "Stop"
 
         # You may have to run this manually
-        Write-Host "Applying 'RemoteSigned' execution policy."
+        Write-Host "`nApplying 'RemoteSigned' execution policy."
         Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 
         # Install Chocolatey package manager
-        if(!(IsInstalled choco)) 
-        {
-            Write-Host "Installing Chocolatey package manager."
+        if(!(IsInstalled choco)) {
+            Write-Host "`nInstalling Chocolatey package manager."
             # Download and run install script
             $ChocoURL = 'https://chocolatey.org/install.ps1'
             Invoke-WebRequest $ChocoURL -UseBasicParsing | Invoke-Expression
             # Load Chocolatey environment variables
             RefreshEnv
+        } else {
+            Write-Host "`nChocolatey is already installed"
         }
 
         # Install git
         if (!(IsInstalled git)) {
+            Write-Host "`nInstalling Git"
             choco install git -y
             # Setup Git remote?
+        } else {
+            Write-Host "`nGit is already installed"
         }
 
         # Install Python 2.7
         if (!(IsInstalled python)) {
+            Write-Host "`nInstalling Python"
             choco install python2 -y
+        } else {
+            Write-Host "`nPython is already installed"
         }
 
         # Refresh PATH environment variable
         $env:Path = [Environment]::GetEnvironmentVariable("Path","Machine")
 
         if (!(IsInstalled virtualenv)) {
+            Write-Host "`nInstalling Virtualenv"
             pip install virtualenv
+        } else {
+            Write-Host "`nVirtualenv is already installed"
         }
 
         if (-Not (Test-Path ./scripts/env/)) {
-            Write-Host "Setting up Python virtual environment"
+            Write-Host "`nSetting up Python virtual environment"
             virtualenv ./scripts/env/            
+        } else {
+            Write-Host "`nVirtual environment already set up"
         }
-        Write-Host "Installing Python requirements"
+
+        Write-Host "`nInstalling Python requirements"
         ./scripts/env/scripts/activate
         pip install -r ./scripts/reqs.txt
-        Write-Host "Done!"
+        Write-Host "`nDone!`n"
     }
     'build' {
         Write-Host "Building Hellhole...`n"
@@ -65,6 +78,15 @@ switch ($command) {
         git add .
         git commit -m "Auto commit"
         git push
+        Write-Host "`nDone!`n"
+    }
+    'lint' {
+        Write-Host "Formatting source HTML...`n"
+        ./scripts/env/scripts/activate
+        ForEach ($file in (Get-ChildItem .\source\)) {
+            $path = Join-Path .\source\ $file
+            python ./scripts/env/Scripts/html5-print $path
+        }
     }
     default {
         Write-Host "`n`tHellhole Shortcuts`n"
